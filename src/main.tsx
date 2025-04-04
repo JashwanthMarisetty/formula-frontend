073942ec3dbe1f4837208320625017f5d1820c81
Codewizard-18 <jaswanthm811@gmail.com>
2025-04-05 02:20:07 +0530

    added login and athentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from "@auth0/auth0-react"; 
+import { Domain,Client } from './helper.tsx';
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <Auth0Provider
+      domain={Domain} 
+      clientId={Client} 
+      authorizationParams={{
+        redirect_uri: "http://localhost:8080", 
+        audience: "https://dev-sfiofywscwujl5yh.us.auth0.com/api/v2/",
+      }}
+      cacheLocation="localstorage"
+    >
+        <App />
+    </Auth0Provider>
+  );
